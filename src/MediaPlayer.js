186e33d985fe7b519c592ceccde0e21037fc9500
Sebastian Ruiz <sebastian.ruiz8@udea.edu.co>
2021-02-22 13:38:41 -0500

    Added Init plugins function

--- src/MediaPlayer.js
+++ src/MediaPlayer.js
@@ -1,13 +1,42 @@
 function MediaPlayer(config) {
   this.media = config.el;
   this.plugins = config.plugins || [];
+  this.initPlugins();
 }
+
+MediaPlayer.prototype.initPlugins = function () {
+  this.plugins.forEach((plugin) => {
+    plugin.run(this);
+  });
+};
+
+MediaPlayer.prototype.play = function () {
+  this.media.play();
+};
+MediaPlayer.prototype.pause = function () {
+  this.media.pause();
+};
 
 MediaPlayer.prototype.togglePlay = function () {
   if (this.media.paused) {
-    this.media.play();
+    this.play();
   } else {
-    this.media.pause();
+    this.pause();
+  }
+};
+
+MediaPlayer.prototype.mute = function () {
+  this.media.muted = true;
+};
+MediaPlayer.prototype.unmute = function () {
+  this.media.muted = false;
+};
+
+MediaPlayer.prototype.toggleMute = function () {
+  if (this.media.muted) {
+    this.unmute();
+  } else {
+    this.mute();
   }
 };
 
