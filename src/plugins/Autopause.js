26225d87e15b03b421018fbef2399ee010016d5f
Sebastian Ruiz <sebastian.ruiz8@udea.edu.co>
2021-02-25 19:34:48 -0500

    Added AutoPause with VisibilityChange

--- src/plugins/Autopause.js
+++ src/plugins/Autopause.js
@@ -2,7 +2,9 @@ class Autopause {
   constructor() {
     this.threshold = 0.25;
     this.handleIntersection = this.handleIntersection.bind(this);
+    this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
   }
+
   run(player) {
     this.player = player;
     const observer = new IntersectionObserver(this.handleIntersection, {
@@ -10,6 +12,17 @@ class Autopause {
     });
 
     observer.observe(player.media);
+
+    document.addEventListener("visibilitychange", this.handleVisibilityChange);
+  }
+
+  handleVisibilityChange() {
+    const isVisible = document.visibilityState === "visible";
+    if (isVisible) {
+      this.player.play();
+    } else {
+      this.player.pause();
+    }
   }
 
   handleIntersection(entries) {
